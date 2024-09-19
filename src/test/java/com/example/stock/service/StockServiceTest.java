dc3e26bb7c48577a9dd7d549c0318b6806507cf1
Yeseo Kim <chinkl850@gmail.com>
2024-09-19 23:10:13 +0900

    test: 동시성 문제 - Race Condition

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -9,10 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StockServiceTest {
@@ -43,5 +44,32 @@ class StockServiceTest {
         // then
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+
+    @Test
+    @DisplayName("동시에 100개의 재고 감소 요청이 발생")
+    public void decreaseConcurrently() throws InterruptedException {
+        // given
+        int threadCount = 100; // 멀티 스레드 사용
+        ExecutorService executorService = Executors.newFixedThreadPool(32); // 비동기로 실행하는 작업을 단순화하여 사용할 수 있게 도와주는 Java의 API
+        CountDownLatch latch = new CountDownLatch(threadCount); // 다른 Thread에서 수행 중인 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        // then
+        assertThat(stock.getQuantity()).isEqualTo(0);
     }
 }
